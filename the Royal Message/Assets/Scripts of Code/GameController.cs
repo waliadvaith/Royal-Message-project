6e9db826f6dbbe6d4af7563799f79dd6e8ba93b0
Jonah-blue-flower <williamsj37@go.lisd.net>
2026-04-09 14:43:54 -0500

    Changes
    I added new files to sort out all files and created a Game Controller Script.

---
+++ the Royal Message/Assets/Scripts of Code/GameController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class GameController : MonoBehaviour
+{
+    Camera main;
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
