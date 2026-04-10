a352dc1b25e88737abc2718daf6a93e42a63428f
Lathan444 <bignerl@go.lisd.net>
2026-04-10 13:39:55 -0500

    roll (WIP)

---
+++ the Royal Message/Assets/Scripts/roll.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class roll : MonoBehaviour
+{
+    public bool isInvincible = false;
+    public float rollCooldown = 2f;
+    private float nextRollTime = 0f;
+    private IEnumerable BecomeInvincible()
+    {
+        isInvincible = true;
+        // Optional: Add flashing/transparency code here to show invincibility
+        yield return new WaitForSeconds(1.0f); // Wait for 1 second
+        isInvincible = false;
+    }
+
+    void Update()
+    {
+        // Check for input and if cooldown has elapsed
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time >= nextRollTime)
+        {
+            Roll();
+            IEnumerable(BecomeInvincible());
+            // Reset next available time
+            nextRollTime = Time.time + rollCooldown;
+        }
+    }
+
+    private void IEnumerable(IEnumerable enumerable)
+    {
+        throw new NotImplementedException();
+    }
+
+    void Roll()
+    {
+        Debug.Log("Rolling!");
+        // Add your roll physics/animation here
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && !isInvincible)
+        {
+            // Take Damage
+        }
+    }
+}
+
