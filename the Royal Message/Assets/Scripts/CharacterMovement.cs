b562279f686dfb35ce8f74df447c03419dae5887
Lathan444 <bignerl@go.lisd.net>
2026-04-10 14:15:08 -0500

    did a bunch of random

--- the Royal Message/Assets/Scripts/CharacterMovement.cs
+++ the Royal Message/Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,7 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 5.0f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    public float rotationSpeed = 720f;
 
     void Start()
     {
@@ -14,9 +15,18 @@ public class CharacterMovement : MonoBehaviour
 
     void Update()
     {
-        // Get input in Update for responsiveness
-        movement.x = Input.GetAxisRaw("Horizontal");
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (moveInput != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngle);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    
+    // Get input in Update for responsiveness
+    movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
     }
 
     void FixedUpdate()
