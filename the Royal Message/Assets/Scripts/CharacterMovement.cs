8fe98dfe25d15dcc1725e80b31f97bebde61a197
waliadvaith <walia@go.lisd.net>
2026-04-10 13:53:56 -0500

    Made a functioning tilemap for experiment

--- the Royal Message/Assets/Scripts/CharacterMovement.cs
+++ the Royal Message/Assets/Scripts/CharacterMovement.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        // Get the Rigidbody2D component from the player object
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxisRaw("Horizontal")*Time.deltaTime*speed, Input.GetAxisRaw("Vertical")*Time.deltaTime*speed, 0);
+        // Get input in Update for responsiveness
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement to the Rigidbody in FixedUpdate (physics-safe)
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 }
