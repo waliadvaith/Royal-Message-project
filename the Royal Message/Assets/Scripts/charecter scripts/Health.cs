eb3b8644c1df0addad6efcd7fdca9b6ecd5ffe95
waliadvaith <walia@go.lisd.net>
2026-04-17 15:03:50 -0500

    Added the new animation
    It is slightly broken, will need to fix after

--- the Royal Message/Assets/Scripts/charecter scripts/Health.cs
+++ the Royal Message/Assets/Scripts/charecter scripts/Health.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [Header("Settings")]
-    public float maxHealth = 100f;
+    public float maxHealth = 999f;
     public float currentHealth;
 
     [Header("UI Connection")]
