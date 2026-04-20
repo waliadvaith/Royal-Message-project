559237eba45c3ea60384e5537acfdecb3eb50793
Lathan444 <bignerl@go.lisd.net>
2026-04-20 14:18:52 -0500

    [

--- the Royal Message/Assets/Scripts/charecter scripts/morality.cs
+++ the Royal Message/Assets/Scripts/charecter scripts/morality.cs
@@ -3,8 +3,7 @@ using UnityEngine.UI;
 
 public class morality : MonoBehaviour 
 {
-    // -100 (Evil) to 100 (Good)
-    public float currentMorality = 0f;
+    public float currentMorality = 40;
     public float maxMorality = 100f;
 
     // Reference to a UI Slider component
@@ -21,7 +20,7 @@ public class morality : MonoBehaviour
     {
         if (moralitySlider != null)
         {
-            // Normalize value to 0-1 range for slider
+            moralitySlider.value = currentMorality;
             moralitySlider.value = (currentMorality + maxMorality) / (2 * maxMorality);
         }
     }
