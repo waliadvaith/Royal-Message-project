de34016064df6a7ceaf321d581f5e59aab26191a
Lathan444 <bignerl@go.lisd.net>
2026-04-20 13:51:18 -0500

    codes

--- the Royal Message/Assets/Scripts/charecter scripts/morality.cs
+++ the Royal Message/Assets/Scripts/charecter scripts/morality.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class morality : MonoBehaviour
+public class morality : MonoBehaviour 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // -100 (Evil) to 100 (Good)
+    public float currentMorality = 0f;
+    public float maxMorality = 100f;
+
+    // Reference to a UI Slider component
+    public Slider moralitySlider;
+
+    public void AddMorality(float amount)
     {
-        
+        currentMorality += amount;
+        currentMorality = Mathf.Clamp(currentMorality, -maxMorality, maxMorality);
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateUI()
     {
-        
+        if (moralitySlider != null)
+        {
+            // Normalize value to 0-1 range for slider
+            moralitySlider.value = (currentMorality + maxMorality) / (2 * maxMorality);
+        }
     }
 }
