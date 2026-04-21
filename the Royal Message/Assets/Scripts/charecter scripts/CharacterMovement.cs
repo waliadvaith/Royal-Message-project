92aa46b764963adb41b81d7e01e1f244b13609c7
Lathan444 <bignerl@go.lisd.net>
2026-04-21 13:46:45 -0500

    fixing stuff

--- the Royal Message/Assets/Scripts/charecter scripts/CharacterMovement.cs
+++ the Royal Message/Assets/Scripts/charecter scripts/CharacterMovement.cs
@@ -1,12 +1,15 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CharacterMovementSideScroller : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float switchRate = 0.5f; // Seconds between frames
+    private float nextSwitch;
 
     [Header("Movement Limits")]
-    public float yMin = -2.0f; 
-    public float yMax = 0.5f;  
+    public float yMin = -2.0f;
+    public float yMax = 0.5f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -22,19 +25,28 @@ public class CharacterMovementSideScroller : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        
+        {
+            if (Input.GetKey(KeyCode.RightArrow) && Time.time > nextSwitch)
+            {
+                nextSwitch = Time.time + switchRate;
+            }
+        }
 
     }
 
     void FixedUpdate()
     {
-        
+
         Vector2 targetPosition = rb.position + movement.normalized * speed * Time.fixedDeltaTime;
 
-        
+
         targetPosition.y = Mathf.Clamp(targetPosition.y, yMin, yMax);
 
-        
+
         rb.MovePosition(targetPosition);
+
+
+
+
     }
 }
