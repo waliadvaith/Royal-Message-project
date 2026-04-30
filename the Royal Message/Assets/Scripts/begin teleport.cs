02c691d276391e29d0eaa56054f3b8708937a8dd
Lathan444 <bignerl@go.lisd.net>
2026-04-30 14:54:01 -0500

    blaaaaaaa

--- the Royal Message/Assets/Scripts/begin teleport.cs
+++ the Royal Message/Assets/Scripts/begin teleport.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class beginteleport : MonoBehaviour
 { 
-    // Make sure the function is PUBLIC so the button can see it
     public void LoadTargetScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
