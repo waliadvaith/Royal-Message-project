57cf33585a23675c5cda33d6545782fb41d6d5b4
Jonah-blue-flower <williamsj37@go.lisd.net>
2026-05-06 13:10:28 -0500

    Overtime!

--- the Royal Message/Assets/S-Jonah Scenes/Jonah's Scripts/Speed Overtime.cs
+++ the Royal Message/Assets/S-Jonah Scenes/Jonah's Scripts/Speed Overtime.cs
@@ -4,13 +4,14 @@ using UnityEngine.Rendering;
 
 public class SpeedOvertime : MonoBehaviour
 {
-    // public varaibles that help with speed
-    private float additionalspeed;
+    // public varaibles to get componets
     public CharacterMovement CharacterMovementScript;
     public SpriteRenderer characterSR;
+    public EnemyAI EnemyAIScript;
+    public EnemyAI EnemyAI;
     private SpriteRenderer rb;
+    // time componet
     public float time = 0.00f;
-    public float increase;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,6 +20,7 @@ public class SpeedOvertime : MonoBehaviour
         characterSR = GetComponent<SpriteRenderer>();
         rb = GetComponent<SpriteRenderer>();
         CharacterMovementScript = characterSR.GetComponent<CharacterMovement>();
+        EnemyAIScript = characterSR.GetComponent<EnemyAI>();
     }
 
     // Update is called once per frame
@@ -26,21 +28,40 @@ public class SpeedOvertime : MonoBehaviour
     {
         // the time scale
         time += Time.deltaTime;
-
-        
-        // happens if it has reached a current amount of time
-        if(time > 20.0f)
+        // this is supposed to cap the speed to not go too fast
+        if (CharacterMovementScript.speed > 22.5f)
         {
-            increase = 3.0f * Time.deltaTime;
-            CharacterMovementScript.speed = increase * Time.deltaTime;
-            Debug.Log("you been granted even more speed");
+            CharacterMovementScript.speed = 22.5f;
+            EnemyAIScript.speed += 1.25f * Time.deltaTime;
+                if(EnemyAIScript.speed > 23.0f)
+                {
+                    EnemyAIScript.speed = 23.0f;
+                }
         }
+
+        // happens if it has reached a current amount of time
+        if (time >= 60.0f)
+        {
+             
+            CharacterMovementScript.speed += 3 * Time.deltaTime;
+            Debug.Log("Gotta Go fast. Gotta go fast");
+        }
+            if(time >= 30.0f)
+            {
+                CharacterMovementScript.speed += 2.5f * Time.deltaTime;
+                Debug.Log("you been granted ultra speed");
+            }
+        
+                if(time > 20.0f)
+                {
+                    CharacterMovementScript.speed += 2 * Time.deltaTime;
+                    Debug.Log("you been granted even more speed");
+                }
             if(time >= 10.0f)
             {
-                    if(time < 20.0f)
+                    if(time <= 20.0f)
                     {
-                        increase = 2.0f * Time.deltaTime;
-                        CharacterMovementScript.speed = increase * Time.deltaTime;
+                        CharacterMovementScript.speed += 1.5f * Time.deltaTime;
                         Debug.Log("you been granted more speed");
                     }
                 
