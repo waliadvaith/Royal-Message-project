4c0113f7354f14232d12be970c997347b1a2a7aa
waliadvaith <walia@go.lisd.net>
2026-05-12 13:45:30 -0500

    updoot

--- the Royal Message/Assets/Scripts/charecter scripts/LevelTeleporter.cs
+++ the Royal Message/Assets/Scripts/charecter scripts/LevelTeleporter.cs
@@ -6,6 +6,7 @@ public class LevelTeleporter : MonoBehaviour
 {
     public string sceneToLoad;
     public Vector3 spawnPositionInNewScene;
+    public GameObject HUD;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +15,7 @@ public class LevelTeleporter : MonoBehaviour
             // Start the sequence and don't let the player die
             DontDestroyOnLoad(other.transform.root.gameObject);
             DontDestroyOnLoad(transform.root.gameObject);
+            DontDestroyOnLoad(HUD);
             StartCoroutine(TeleportSequence(other.transform.root.gameObject));
         }
     }
